91c1f3d49fde947bec2302a20ebce71a719af11f
Srayan Jana <valorzard.kiko@outlook.com>
2024-10-09 16:22:22 -0700

    add ebiten stuff

--- main.go
+++ main.go
@@ -13,7 +13,70 @@ import (
 	"os"
 	"strings"
 	"time"
+	_ "image/png"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
+
+var img *ebiten.Image
+
+var (
+	pos_x = 80.0
+	pos_y = 80.0
+)
+
+func init() {
+	var err error
+	img, _, err = ebitenutil.NewImageFromFile("gopher.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// implements ebiten.Game interface
+type Game struct{}
+
+// called every tick (default 60 times a second)
+// updates game logical state
+func (g *Game) Update() error {
+
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		pos_y -= 1
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		pos_y += 1
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		pos_x -= 1
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		pos_x += 1
+	}
+
+	// if update returns non nil error, game suspends
+	return nil
+}
+
+// called every frame, depends on the monitor refresh rate
+// which will probably be at least 60 times per second
+func (g *Game) Draw(screen *ebiten.Image) {
+	// prints something on the screen
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+
+	// draw image
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(pos_x, pos_y)
+	screen.DrawImage(img, op)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 640, 480
+}
 
 var (
 	// probably move all webrtc networking stuff to a struct i can manage
@@ -46,10 +109,13 @@ func startConnection(isHost bool) {
 	}
 
 	// Create a new RTCPeerConnection using the API object
-	peerConnection, err := api.NewPeerConnection(config)
+	pc, err := api.NewPeerConnection(config)
 	if err != nil {
 		panic(err)
 	}
+
+	// Set the global variable to the newly created RTCPeerConnection
+	peerConnection = pc
 
 	// Set the handler for Peer connection state
 	// This will notify you when the peer has connected/disconnected
@@ -133,9 +199,6 @@ func startConnection(isHost bool) {
 
 		// Output the answer in base64 so we can paste it in browser
 		fmt.Println(encode(peerConnection.LocalDescription()))
-
-		// Block forever
-		select {}
 	} else {
 		// Create a datachannel with label 'data'
 		dataChannel, err := peerConnection.CreateDataChannel("data", nil)
@@ -186,9 +249,6 @@ func startConnection(isHost bool) {
 		if err := peerConnection.SetRemoteDescription(descr); err != nil {
 			panic(err)
 		}
-
-		// Block forever
-		select {}
 	}
 }
 
@@ -204,6 +264,15 @@ func main() {
 	isHost := len(argsWithProg) > 1 && argsWithProg[1] == "host"
 	startConnection(isHost)
 	defer closeConnection()
+
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+
+	// triggers the game loop to actually start up
+	// if we run into an error, log what it is
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
 
 // ReadLoop shows how to read from the datachannel directly
