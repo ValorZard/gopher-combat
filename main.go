5902b108ff7a4366e81f303d63a0344dc69f6fd4
Srayan Jana <valorzard.kiko@outlook.com>
2024-10-15 14:53:49 -0700

    add text box

--- main.go
+++ main.go
@@ -53,6 +53,9 @@ func init() {
 type Game struct{
 	ui  *ebitenui.UI
 	btn *widget.Button
+	//This parameter is so you can keep track of the textInput widget to update and retrieve
+	//its values in other parts of your game
+	standardTextInput *widget.TextInput
 }
 
 // Layout implements Game.
@@ -397,7 +400,7 @@ func main() {
 			// instruct the container's anchor layout to center the button both horizontally and vertically
 			widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
 				HorizontalPosition: widget.AnchorLayoutPositionCenter,
-				VerticalPosition:   widget.AnchorLayoutPositionCenter,
+				VerticalPosition:   widget.AnchorLayoutPositionCenter + 20,
 			}),
 		),
 		// specify the images to use
@@ -466,17 +469,69 @@ func main() {
 	// add the button as a child of the container
 	rootContainer.AddChild(button)
 
+	// construct a standard textinput widget
+	standardTextInput := widget.NewTextInput(
+		widget.TextInputOpts.WidgetOpts(
+			//Set the layout information to center the textbox in the parent
+			widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+				HorizontalPosition: widget.AnchorLayoutPositionCenter,
+				VerticalPosition:   widget.AnchorLayoutPositionCenter,
+			}),
+		),
+
+		//Set the Idle and Disabled background image for the text input
+		//If the NineSlice image has a minimum size, the widget will use that or
+		// widget.WidgetOpts.MinSize; whichever is greater
+		widget.TextInputOpts.Image(&widget.TextInputImage{
+			Idle:     image.NewNineSliceColor(color.NRGBA{R: 100, G: 100, B: 100, A: 255}),
+			Disabled: image.NewNineSliceColor(color.NRGBA{R: 100, G: 100, B: 100, A: 255}),
+		}),
+
+		//Set the font face and size for the widget
+		widget.TextInputOpts.Face(face),
+
+		//Set the colors for the text and caret
+		widget.TextInputOpts.Color(&widget.TextInputColor{
+			Idle:          color.NRGBA{254, 255, 255, 255},
+			Disabled:      color.NRGBA{R: 200, G: 200, B: 200, A: 255},
+			Caret:         color.NRGBA{254, 255, 255, 255},
+			DisabledCaret: color.NRGBA{R: 200, G: 200, B: 200, A: 255},
+		}),
+
+		//Set how much padding there is between the edge of the input and the text
+		widget.TextInputOpts.Padding(widget.NewInsetsSimple(5)),
+
+		//Set the font and width of the caret
+		widget.TextInputOpts.CaretOpts(
+			widget.CaretOpts.Size(face, 2),
+		),
+
+		//This text is displayed if the input is empty
+		widget.TextInputOpts.Placeholder("Standard Textbox"),
+
+		//This is called when the user hits the "Enter" key.
+		//There are other options that can configure this behavior
+		widget.TextInputOpts.SubmitHandler(func(args *widget.TextInputChangedEventArgs) {
+			fmt.Println("Text Submitted: ", args.InputText)
+		}),
+
+		//This is called whenver there is a change to the text
+		widget.TextInputOpts.ChangedHandler(func(args *widget.TextInputChangedEventArgs) {
+			fmt.Println("Text Changed: ", args.InputText)
+		}),
+	)
+
+	rootContainer.AddChild(standardTextInput)
+
 	// construct the UI
 	ui := ebitenui.UI{
 		Container: rootContainer,
 	}
 
-	// Ebiten setup
-	ebiten.SetWindowTitle("Ebiten UI - Buttons")
-
 	game := Game{
 		ui:  &ui,
 		btn: button,
+		standardTextInput: standardTextInput,
 	}
 
 	// triggers the game loop to actually start up
