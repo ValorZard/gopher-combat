dd3112306dd4691ff8d4694a3d42a8777b2892ce
Srayan Jana <valorzard.kiko@outlook.com>
2025-03-23 20:47:59 -0700

    change build script, dunno if this will still work tbh

--- build_wasm.ps1
+++ build_wasm.ps1
@@ -5,7 +5,9 @@ Remove-Item Env:GOOS
 Remove-Item Env:GOARCH
 
 $goroot = go env GOROOT
-cp $goroot\misc\wasm\wasm_exec.js .
+# have to copy the wasm_exec.js file to the current directory
+# not really sure where it should be, but this is where it is on my computer
+cp $goroot\lib\wasm\wasm_exec.js .
 
 # serve the files
 python3 -m http.server
